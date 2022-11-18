739ea131776a78d9b0f2ff1207e375486d41cc16
Devesh Wadhwani <devesh.wadhwani123@gmail.com>
2022-11-18 15:34:52 +0000

    Update 6 files

---
+++ src/pages/predict.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Predict = () => {
+  return (
+    <div>
+      <div className=" font-bold">
+      <form className=' text-center justify-center'>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+     </div>
+    </div>
+  )
+}
+
+export default Predict
